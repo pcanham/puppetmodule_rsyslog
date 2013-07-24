08f64d6f153533767ff2cc0cc1b656b9827fa9e8
Paul Canham <pcanham@cambridge.org>
2013-07-24 15:39:54 +0100

    trying to fix install section of module

--- manifests/yumrepo.pp
+++ manifests/yumrepo.pp
@@ -19,7 +19,6 @@ class rsyslog::yumrepo {
     gpgkey => 'http://rpms.adiscon.com/RPM-GPG-KEY-Adiscon',
     enabled => '0',
     gpgcheck => '0',
-    notify   => Exec['rsyslog-yum-clean-all'],
   }->
     yumrepo { 'rsyslog-v6-stable':
       baseurl => "http://rpms.adiscon.com/v6-stable/epel-6/${::architecture}",
@@ -27,7 +26,6 @@ class rsyslog::yumrepo {
       gpgkey => 'http://rpms.adiscon.com/RPM-GPG-KEY-Adiscon',
       enabled => '0',
       gpgcheck => '0',
-      notify   => Exec['rsyslog-yum-clean-all'],
     }->
       yumrepo { 'rsyslog-v7-stable':
         baseurl => "http://rpms.adiscon.com/v7-stable/epel-6/${::architecture}",
@@ -35,11 +33,5 @@ class rsyslog::yumrepo {
         gpgkey => 'http://rpms.adiscon.com/RPM-GPG-KEY-Adiscon',
         enabled => '1',
         gpgcheck => '0',
-        notify   => Exec['rsyslog-yum-clean-all'],
-      }->
-        exec { 'rsyslog-yum-clean-all': 
-          path => '/bin:/usr/bin:/usr/sbin:/usr/local/bin',
-          subscribe => [ Yumrepo['rsyslog-v5-stable'], Yumrepo['rsyslog-v6-stable'], Yumrepo['rsyslog-v7-stable'] ], 
-          command => "yum clean all" 
-        }
+      }
 }
