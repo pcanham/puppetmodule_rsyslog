c35afa820c722b63378daa9177adceaac9cecc6b
Paul Canham <paul@canham.eu>
2013-07-27 20:25:49 +0100

    adding before state

--- manifests/yumrepo.pp
+++ manifests/yumrepo.pp
@@ -17,7 +17,9 @@ class rsyslog::yumrepo {
   if defined(Package['redhat-lsb']) {
     notice("Package: redhat-lsb already defined.")
   } else {
-    package {'redhat-lsb': }
+    package {'redhat-lsb':
+      before => Package['redhat-lsb'] 
+    }
   }
   
   yumrepo { "rsyslog-v${rsyslog::rsyslogmjrver}-stable":
